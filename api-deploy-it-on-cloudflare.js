81451712080b1961e7b38397aa1161fbfa567f9f
rastynbkhty11-a11y <rastynbkhty11@gmail.com>
2026-08-08 00:33:40 +0330

    Update api-deploy-it-on-cloudflare.js

--- api-deploy-it-on-cloudflare.js
+++ api-deploy-it-on-cloudflare.js
@@ -3,7 +3,7 @@ const CONFIG = {
     TIMEOUT: 3000,
     MAX_IPS_PER_REQUEST: 200,
     CACHE_TTL: 300,
-    EXIT_ADDRESSES_URL: 'https://check.torproject.org/exit-addresses',
+    EXIT_ADDRESSES_URL: 'https://small-base-6f54.rastynbkhty11.workers.dev',
     ONIONOO_API_URL: 'https://onionoo.torproject.org/details?search=flag:exit',
     IP_API_URL: 'http://ip-api.com/json/',
     IP_API_BATCH_URL: 'http://ip-api.com/batch',
